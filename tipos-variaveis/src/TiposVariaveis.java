cd4e5b1285d068c499ec6e8edce55172f8916429
Luiz-Eduardo1985 <luiz.e.rodrigues.1985@gmail.com>
2024-04-27 18:19:25 -0300

    exemplos de aula

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,24 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        double salarioMinimo = 2500;
+        int numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;// erro pois numero normal pode ser maior que short suporta
+
+        int numero = 5;
+        numero = 10;
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;// expressão "final" impede que a variavel troque de valor
+        // nome da variavel toda em maiuscula é uma convenção para demonstrar que é um
+        // valor constante.
+        int numero1 = 1;
+        int numero2 = 1;
+
+        boolean simNao = numero1 == numero2;
+
+        System.out.println(simNao);
+
     }
 }
