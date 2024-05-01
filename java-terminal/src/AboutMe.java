96d72958f781a3af036a2f3064ef52a2e62c096e
Luiz-Eduardo1985 <luiz.e.rodrigues.1985@gmail.com>
2024-05-01 16:48:28 -0300

    exercicio try catch - about me

--- java-terminal/src/AboutMe.java
+++ java-terminal/src/AboutMe.java
@@ -1,33 +1,39 @@
+import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class AboutMe {
     public static void main(String[] args) {
-        // criando objeto scanner
-        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        System.out.println("Digite seu nome:");
-        String nome = scanner.next();
+        try {
+            // criando objeto scanner
+            Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        System.out.println("Digite seu sobrenome:");
-        String sobrenome = scanner.next();
+            System.out.println("Digite seu nome:");
+            String nome = scanner.next();
 
-        System.out.println("Digite sua idade:");
-        int idade = scanner.nextInt();
+            System.out.println("Digite seu sobrenome:");
+            String sobrenome = scanner.next();
 
-        System.out.println("Digite a sua altura:");
-        double altura = scanner.nextDouble();
+            System.out.println("Digite sua idade:");
+            int idade = scanner.nextInt();
 
-        // String nome = args[0];
-        // String sobrenome = args[1];
-        // int idade = Integer.valueOf(args[2]);// vamos falar sobre
-        // double altura = Double.valueOf(args[3]);
+            System.out.println("Digite a sua altura:");
+            double altura = scanner.nextDouble();
 
-        System.out.println("Olá, me chamo " + nome + " " + sobrenome);
-        System.out.println("Tenho " + idade + "anos.");
-        System.out.println("Minha altura é " + altura + "m");
+            // String nome = args[0];
+            // String sobrenome = args[1];
+            // int idade = Integer.valueOf(args[2]);// vamos falar sobre
+            // double altura = Double.valueOf(args[3]);
 
-        scanner.close();
+            System.out.println("Olá, me chamo " + nome + " " + sobrenome);
+            System.out.println("Tenho " + idade + "anos.");
+            System.out.println("Minha altura é " + altura + "m");
+
+            scanner.close();
+        } catch (InputMismatchException e) {
+            System.err.println("Os campos idade e altura, precisam ser númericos.");
+        }
 
     }
 
